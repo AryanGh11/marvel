4b79180c687ef2ca9b0100396882ea4e7a727e6a
Aryan Gholami <mahdi.gholami875@gmail.com>
2023-09-25 18:43:10 +0330

    try to set up pwa

--- next.config.js
+++ next.config.js
@@ -18,17 +18,28 @@ const nextConfig = {
     config.resolve = {
       ...config.resolve,
       fallback: {
-        "fs": false,
-        "path": false,
-        "os": false,
-        "net": false,
-        "dns": false,
-        "child_process": false,
-        "tls": false
-      }
-    }
-    return config
+        fs: false,
+        path: false,
+        os: false,
+        net: false,
+        dns: false,
+        child_process: false,
+        tls: false,
+      },
+    };
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/",
+  },
+});
+
+module.exports = nextConfig;
