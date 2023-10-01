6ed3480700f38f81b9a10fc5145102fdda0a144d
Aryan Gholami <mahdi.gholami875@gmail.com>
2023-10-01 15:02:08 +0330

    fixing loading page z-index

--- app/components/LoadingPage.tsx
+++ app/components/LoadingPage.tsx
@@ -19,7 +19,7 @@ export default function LoadingPage({ timer }: TimerType) {
 
   return (
     !hide && (
-      <motion.div className="w-full h-screen absolute top-0 right-0 bg-primary z-10 flex flex-col gap-6 justify-center items-center ">
+      <motion.div className="w-full h-screen absolute top-0 right-0 bg-primary z-30 flex flex-col gap-6 justify-center items-center ">
         <motion.div
           initial={{ scale: 1 }}
           transition={{ repeat: Infinity, duration: 0.5, repeatType: "mirror" }}
